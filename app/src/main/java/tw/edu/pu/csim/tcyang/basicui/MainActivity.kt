c481b8a987a8e9c8be4627a827fe0eac74d0a98d
chang200601-ubs <chang200601@gmail.com>
2025-10-20 15:06:43 +0800

    v.3

--- app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/basicui/MainActivity.kt
@@ -21,6 +21,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -130,11 +134,22 @@ fun Main(modifier: Modifier = Modifier) {
 
         }
         Spacer(modifier = Modifier.size(10.dp))
+
+        var flag by remember { mutableStateOf(value="text") }
         Button(
-            onClick = {}
+            onClick = {
+                if(flag=="text"){
+                    flag="abc"
+                }
+                else{
+                    flag="text"
+                }
+            }
         ){
             Text(text="歡迎修課")
         }
+
+        Text(text=flag)
 
 
     }
